343781e807f2b2255e9c768032d3ccd8853f072e
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-02-01 17:41:37 +0500

    Undo footer

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer";
 import NavbarSimple from "../components/NavbarSimple";
+import SimpleFooter from "../components/SimpleFooter";
 
 function RootLayout() {
   return (
