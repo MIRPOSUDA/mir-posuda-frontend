7e2e5a418a9dd0dbde690d914b52f066229b07aa
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-02-03 21:05:30 +0500

    Clear junk codes

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./custom-toast.css";
 import "./index.css";
-import { ToastContainer, toast } from "react-toastify";
-import { css } from "glamor";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <App />
-    {toast.warn("Salom")}
-    <ToastContainer bodyClassName={css({ fontFamily: "Roboto" })} limit={3} />
+    <ToastContainer position="top-center" limit={3} />
   </>,
 );
